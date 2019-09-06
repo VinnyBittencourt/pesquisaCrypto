7b6a70eb82a057401c48a454754d54332d25598c
Vinicius <vinisataides@gmail.com>
2019-09-06 10:08:01 -0300

    First Commit

---
+++ PesquisaCrypto/Models/Moedas.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PesquisaCrypto.Models
+{
+    public class Moedas
+    {        
+        public int MoedasId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório.")]
+        [MaxLength(180, ErrorMessage ="Digite menos caracteres.")]
+        [Remote("MoedaExisteAsync", "Moedas")]
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+        [NotMapped]
+        public bool CheckBoxMarcado { get; set; }
+    }
+}
